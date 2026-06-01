6b7621bd9e3e44c6a867c2d9614b97dd189ff575
Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>
2026-06-01 14:07:10 +0800

    entropy: reconstruct entropy HAL
    Signed-off-by: Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>

---
+++ bee/drivers/entropy/inc/rng_interface.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2026, Realtek Semiconductor Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __RNG_INTERFACE_H
+#define __RNG_INTERFACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint32_t platform_random(uint32_t max);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__RNG_INTERFACE_H*/
